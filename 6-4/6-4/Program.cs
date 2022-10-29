fad8123389fce5adfa987806396c28edbf756a55
DmitryKhvostenko <112482754+DmitryKhvostenko@users.noreply.github.com>
2022-10-30 02:37:43 +0300

    Add files via upload

---
+++ 6-4/6-4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace е64
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = MyClass<int>.FactoryMethod();
+            var word = MyClass<double>.FactoryMethod();
+            Console.WriteLine("Значение: " + num + " Тип: " + num.GetType());
+            Console.WriteLine("Значение: " + word + " Тип: " + word.GetType());
+        }
+    }
+}
